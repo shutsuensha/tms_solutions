547526760e176cbc7fb64748578b266766a15f24
shutsuensha <shutsuenshazvezd@gmail.com>
2024-10-25 08:03:07 +0300

    task 5

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
+def caesar_cipher_encrypt(message, shift):
+    encrypted_message = ""
+    for char in message:
+        if char.isalpha():
+            shift_base = ord('A') if char.isupper() else ord('a')
+            encrypted_message += chr((ord(char) - shift_base + shift) % 26 + shift_base)
+        else:
+            encrypted_message += char 
+    return encrypted_message
 
-a = 12
-b = 18
-result = gcd(a, b)
-print(f"НОД чисел {a} и {b}: {result}")
+
+def caesar_cipher_decrypt(message, shift):
+    return caesar_cipher_encrypt(message, -shift) 
+
+
+def caesar_cipher(message, shift, action):
+    if action == "encrypt":
+        return caesar_cipher_encrypt(message, shift)
+    elif action == "decrypt":
+        return caesar_cipher_decrypt(message, shift)
+    else:
+        return "Неверное действие!"
+
+
+message = input("Введите сообщение: ")
+shift = int(input("Введите сдвиг: "))
+action = input("Выберите действие (encrypt или decrypt): ")
+
+result = caesar_cipher(message, shift, action)
+print(f"Результат: {result}")
