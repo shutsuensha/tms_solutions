3dc63339e1bd6ad243aea4eeb2d01b238b3db1f2
shutsuensha <shutsuenshazvezd@gmail.com>
2024-10-25 07:53:29 +0300

    task 1

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-number = 123
+import math
 
-# Sum of digits
-sum_of_digits = sum(int(digit) for digit in str(number))
+a = 2
+b = 4
+y = a**2 / 3 + (a**2 + 4) / b + ((a**2 + 4) ** 0.5) / 4 + ((a**2 + 4)**3)**0.5 / 4
+print(f"{y=}")
 
-print(f"Sum of digits: {sum_of_digits}")
+
+x = 2
+y = (math.cos(x**2)**2 + math.sin(2*x - 1)**2) ** (1/3)
+print(f"{y=}")
+
+y = math.cos(x) + math.sin(x)
+print(f"{y=}")
+
+y = 5*x + 3*x**2*(1 + math.sin(x)**2)**0.5
+print(f"{y=}")
