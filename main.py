7d480792fa786060ec2beadaac648bcb797c2dd4
shutsuensha <shutsuenshazvezd@gmail.com>
2024-10-25 07:59:23 +0300

    task 6

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-def fibonacci_recursive(n):
-    if n == 1 or n == 2:
-        return 1
-    else:
-        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
-
-def print_fibonacci_sequence(n):
-    sequence = [fibonacci_recursive(i) for i in range(1, n+1)]
-    print("Последовательность Фибоначчи:")
-    print(" ".join(map(str, sequence)))
+def list_statistics(numbers):
+    # Находим сумму, минимальное и максимальное значение
+    total_sum = sum(numbers)
+    minimum = min(numbers)
+    maximum = max(numbers)
+    
+    return total_sum, minimum, maximum
 
 
-n = int(input("Сколько чисел Фибоначчи вы хотите вывести? "))
-print_fibonacci_sequence(n)
+numbers = list(map(int, input("Введите список чисел через пробел: ").split()))
+
+total_sum, minimum, maximum = list_statistics(numbers)
+
+# Печатаем результаты
+print(f"Сумма всех чисел: {total_sum}")
+print(f"Минимальное число: {minimum}")
+print(f"Максимальное число: {maximum}")
