4871f9a76e325e1bfa88fcc4ef7e98b6474c1f7d
shutsuensha <shutsuenshazvezd@gmail.com>
2024-10-25 07:59:00 +0300

    task 4

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-def transpose_matrix(matrix):
+def calculate_days_to_save(N, k):
+    result = 0
+    cnt = 6
+    weeks = 0
+    # сколько недель надо маше на покупку, последнюю неделю не считаем
+    while True:
+        result += cnt * k
+        if result > N:
+            result -= cnt * k
+            break
+        weeks += 1
+    days = weeks * 7
+    # количество дней без учета последней недели + кол-во дней
+    while True:
+        result += k
+        days += 1
+        if result >= N:
+            return days
     
-    transposed = [list(row) for row in zip(*matrix)]
-    return transposed
 
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-transposed_matrix = transpose_matrix(matrix)
-
-for row in transposed_matrix:
-    print(row)
+N = int(input("Введите стоимость телефона: "))
+k = int(input("Введите сумму, которую Маша откладывает каждый день: "))
+days = calculate_days_to_save(N, k)
+print(f"Маша накопит на телефон за {days} дней.")
