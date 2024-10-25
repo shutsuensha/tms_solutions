2e5cc6bc9fac2f80c672e94cfc62c7f2c42f6980
shutsuensha <shutsuenshazvezd@gmail.com>
2024-10-25 07:37:37 +0300

    task 3

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import math
+string = "Hello world"
+words_count = len(string.split())
 
-cat_a = 3
-cat_b = 4
-
-# Area of the triangle
-area = (cat_a * cat_b) / 2
-
-# Hypotenuse using Pythagorean theorem
-hypotenuse = math.sqrt(cat_a**2 + cat_b**2)
-
-print(f"Area: {area}, Hypotenuse: {hypotenuse}")
+print(f"Number of words: {words_count}")
