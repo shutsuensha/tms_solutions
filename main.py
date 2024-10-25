3786a711243a1ad343ab831488169a184e4a3cf0
shutsuensha <shutsuenshazvezd@gmail.com>
2024-10-25 08:03:19 +0300

    task 7

--- main.py
+++ main.py
@@ -1,30 +1,16 @@
-def caesar_cipher_encrypt(message, shift):
-    encrypted_message = ""
-    for char in message:
-        if char.isalpha():
-            shift_base = ord('A') if char.isupper() else ord('a')
-            encrypted_message += chr((ord(char) - shift_base + shift) % 26 + shift_base)
-        else:
-            encrypted_message += char 
-    return encrypted_message
+import random
 
+def create_random_matrix(M, N, min_value=0, max_value=100):
+    matrix = []
+    for _ in range(M):
+        row = [random.randint(min_value, max_value) for _ in range(N)]
+        matrix.append(row)
+    return matrix
 
-def caesar_cipher_decrypt(message, shift):
-    return caesar_cipher_encrypt(message, -shift) 
+M = 3  
+N = 4 
+matrix = create_random_matrix(M, N)
 
-
-def caesar_cipher(message, shift, action):
-    if action == "encrypt":
-        return caesar_cipher_encrypt(message, shift)
-    elif action == "decrypt":
-        return caesar_cipher_decrypt(message, shift)
-    else:
-        return "Неверное действие!"
-
-
-message = input("Введите сообщение: ")
-shift = int(input("Введите сдвиг: "))
-action = input("Выберите действие (encrypt или decrypt): ")
-
-result = caesar_cipher(message, shift, action)
-print(f"Результат: {result}")
+# Выводим матрицу
+for row in matrix:
+    print(row)
