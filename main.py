4bdcef5dba29753b1c01069d3ce3a2ed6f6f46c6
shutsuensha <shutsuenshazvezd@gmail.com>
2024-10-25 07:37:46 +0300

    task 4

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-string = "Hello world"
-words_count = len(string.split())
+string = "hhhabchghhh"
 
-print(f"Number of words: {words_count}")
+first_h_index = string.find('h')
+last_h_index = string.rfind('h')
+
+middle_part = string[first_h_index+1:last_h_index].replace('h', 'H')
+result = string[:first_h_index+1] + middle_part + string[last_h_index:]
+
+print(f"Result: {result}")
