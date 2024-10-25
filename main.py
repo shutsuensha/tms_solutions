1fb35548b7f39b6d2e996a9482d1a2991385b701
shutsuensha <shutsuenshazvezd@gmail.com>
2024-10-25 08:03:33 +0300

    task 8

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-import random
+def find_min_max(matrix):
+    min_value = float('inf')
+    max_value = float('-inf')
+    min_index = (-1, -1)
+    max_index = (-1, -1)
+    
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] < min_value:
+                min_value = matrix[i][j]
+                min_index = (i, j)
+            if matrix[i][j] > max_value:
+                max_value = matrix[i][j]
+                max_index = (i, j)
+    
+    return min_value, min_index, max_value, max_index
 
-def create_random_matrix(M, N, min_value=0, max_value=100):
-    matrix = []
-    for _ in range(M):
-        row = [random.randint(min_value, max_value) for _ in range(N)]
-        matrix.append(row)
-    return matrix
+matrix = [
+    [12, 34, 56, 78],
+    [90, 23, 45, 67],
+    [89, 10, 11, 13]
+]
 
-M = 3  
-N = 4 
-matrix = create_random_matrix(M, N)
+min_value, min_index, max_value, max_index = find_min_max(matrix)
 
-# Выводим матрицу
-for row in matrix:
-    print(row)
+print(f"Минимальный элемент: {min_value}, Индексы: {min_index}")
+print(f"Максимальный элемент: {max_value}, Индексы: {max_index}")
