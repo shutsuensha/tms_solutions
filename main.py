13a10e09a1351d88201729ef0afb9e71e3708c38
shutsuensha <shutsuenshazvezd@gmail.com>
2024-10-25 08:05:50 +0300

    task 1

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-def add_even_column(matrix):
-    new_column = []
-    
-    for row in matrix:
-        count_ones = row.count(1)
-        new_column.append(0 if count_ones % 2 == 0 else 1)
-
-    for i in range(len(matrix)):
-        matrix[i].append(new_column[i])
-    
-    return matrix
-
-matrix = [
-    [0, 1, 0],
-    [1, 1, 1],
-    [0, 0, 1],
-    [1, 0, 0]
-]
-
-updated_matrix = add_even_column(matrix)
-
-for row in updated_matrix:
-    print(row)
+numbers = [1, 2, 3]
+string_numbers = list(map(str, numbers))
+print(string_numbers)
