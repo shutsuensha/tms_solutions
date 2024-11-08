da249e61e396ee52bd69009049f7ee29898e7915
shutsuensha <shutsuenshazvezd@gmail.com>
2024-11-08 21:22:09 +0300

    task1 done

---
+++ 13.iter_gen_patterns/task1/main.py
@@ -0,0 +1,14 @@
+def cyclic_sequence(sequence):
+    while True:
+        for number in sequence:
+            yield number
+
+sequence = [1, 2, 3]
+
+generator = cyclic_sequence(sequence)
+
+num_elements = int(input("Enter the number of elements to display: "))
+
+for _ in range(num_elements):
+    print(next(generator), end=" ")
+print()
