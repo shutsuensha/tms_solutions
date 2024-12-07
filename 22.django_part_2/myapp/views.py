77d0303d82983cc214c635964fb96a36e080a146
shutsuensha <shutsuenshazvezd@gmail.com>
2024-12-07 19:47:40 +0300

    create index view for get/post form

--- 22.django_part_2/myapp/views.py
+++ 22.django_part_2/myapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import BookForm
+from .models import Book
 
-# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            Book.objects.create(
+                title=form.cleaned_data['title'],
+                author=form.cleaned_data['author'],
+                publication_date=form.cleaned_data['publication_date'],
+                isbn=form.cleaned_data['isbn'],
+                pages=form.cleaned_data['pages']
+            )
+    else:
+        form = BookForm()
+    return render(request, 'index.html', {'form': form})
