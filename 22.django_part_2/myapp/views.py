73003a3c94b2eb5c6c2e03b6451dbf72ab84ae86
shutsuensha <shutsuenshazvezd@gmail.com>
2024-12-07 20:32:01 +0300

    refactor index view and add views for 2 forms

--- 22.django_part_2/myapp/views.py
+++ 22.django_part_2/myapp/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
-from .forms import BookForm
+from django.shortcuts import render, redirect
 from .models import Book
+from .forms import BookForm, BookFormModel
+
 
 def index(request):
+    books = Book.objects.all()
+    return render(request, 'index.html', {'books': books})
+
+
+def add_book_form_view(request):
     if request.method == 'POST':
         form = BookForm(request.POST)
         if form.is_valid():
@@ -13,6 +19,20 @@ def index(request):
                 isbn=form.cleaned_data['isbn'],
                 pages=form.cleaned_data['pages']
             )
+            return redirect('books')
     else:
         form = BookForm()
-    return render(request, 'index.html', {'form': form})
+
+    return render(request, 'add_book.html', {'form': form})
+
+
+def add_book_form_model_view(request):
+    if request.method == 'POST':
+        form = BookFormModel(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('books')
+    else:
+        form = BookFormModel()
+
+    return render(request, 'add_book.html', {'form': form})
